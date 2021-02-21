2958507da5863bef1c530bbe7d6ce3c1a0cd4458
Tom_The_Geek <23531706+Geek202@users.noreply.github.com>
2021-02-21 09:56:03 +0000

    actually compile lol

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let command = args[0].to_owned();
     args.remove(0);
 
-    let webhook_url = std::env::var("WEBHOOK_URL").unwrap_or_else("");
+    let webhook_url = std::env::var("WEBHOOK_URL").unwrap_or_else(|_e| { String::from("") });
     let webhook = if webhook_url != "" {
-        Some(Webhook::from_url(&*webhook_url));
+        Some(Webhook::from_url(&*webhook_url))
     } else {
         None
     };
 
-    send_message(&*webhook, "Starting server...").await?;
+    send_message(&webhook, "Starting server...").await?;
 
     loop {
         let start_time = Instant::now();
@@ -36,43 +36,43 @@ async fn main() -> Result<(), Box<dyn Error>> {
             if was_restart {
                 if too_fast {
                     let pause_time = RESTART_INTERVAL - runtime;
-                    send_message(&*webhook, &*format!("Server exited after {:?}s! Waiting {:?}s before restarting...", runtime.as_secs(), pause_time.as_secs()));
+                    send_message(&webhook, &*format!("Server exited after {:?}s! Waiting {:?}s before restarting...", runtime.as_secs(), pause_time.as_secs())).await?;
                     tokio::time::delay_for(pause_time).await;
                 }
 
                 let restart_reason = fs::read_to_string(".restart_reason").expect("Failed to read restart reason!");
-                send_message(&*webhook, &*("Restarting server as it was requested!\nReason: ".to_owned() + &restart_reason)).await?;
+                send_message(&webhook, &*("Restarting server as it was requested!\nReason: ".to_owned() + &restart_reason)).await?;
                 fs::remove_file(".restart_reason").expect("failed to remove restart reason file!");
             } else {
-                send_message(&*webhook, "Server exited normally, not restarting!").await?;
+                send_message(&webhook, "Server exited normally, not restarting!").await?;
                 break;
             }
         } else if exit_code == 0xFFAAFF {
             if too_fast {
                 let pause_time = RESTART_INTERVAL - runtime;
-                send_message(&*webhook, &*format!("Server exited after {:?}s! Waiting {:?}s before restarting...", runtime.as_secs(), pause_time.as_secs()));
+                send_message(&webhook, &*format!("Server exited after {:?}s! Waiting {:?}s before restarting...", runtime.as_secs(), pause_time.as_secs())).await?;
                 tokio::time::delay_for(pause_time).await;
             }
 
-            send_message(&*webhook, "Server didn't return an exit code? Assuming hard crash and restarting...").await?;
+            send_message(&webhook, "Server didn't return an exit code? Assuming hard crash and restarting...").await?;
         } else {
             if too_fast {
                 let pause_time = RESTART_INTERVAL - runtime;
-                send_message(&*webhook, &*format!("Server exited after {:?}s! Waiting {:?}s before restarting...", runtime.as_secs(), pause_time.as_secs()));
+                send_message(&webhook, &*format!("Server exited after {:?}s! Waiting {:?}s before restarting...", runtime.as_secs(), pause_time.as_secs())).await?;
                 tokio::time::delay_for(pause_time).await;
             }
 
-            send_message(&*webhook, "Server exited with non-zero exit code, restarting...").await?;
+            send_message(&webhook, "Server exited with non-zero exit code, restarting...").await?;
         }
     }
 
     Ok(())
 }
 
-async fn send_message(webhook: Option<Webhook>, msg: &str) -> Result<(), Box<dyn Error>> {
+async fn send_message(webhook: &Option<Webhook>, msg: &str) -> Result<(), Box<dyn Error>> {
     match webhook {
         Some(webhook) => webhook.send(|message| message.content(msg)).await,
-        _ => {}
+        _ => Ok(())
     }
 }
 
